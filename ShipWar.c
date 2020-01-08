9d094ab8f482d5a1d160863c0c8cc13fc156763a
AsenikDev <asenik.dev@gmail.com>
2020-01-08 23:45:59 +0100

    Adding anonyme structure Ship and PartShip

---
+++ ShipWar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+  bool downed;
+  ShipPart * partList;
+} Ship;
+typedef struct {
+  bool touched;
+  Ship * ship;
+} ShipPart ShipPart;
+
+
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
