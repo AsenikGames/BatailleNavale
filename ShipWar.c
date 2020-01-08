ef55809e751afb580bef54bac0105546856ae680
AsenikDev <asenik.dev@gmail.com>
2020-01-09 00:47:25 +0100

    Add body to method to test program

--- ShipWar.c
+++ ShipWar.c
@@ -6,8 +6,8 @@ typedef struct ShipPart ShipPart;
 
 // New data Type Ship representing a Ship in the game
 struct Ship{
-  bool downed;          // True if ship has been downed
-  ShipPart * partList;  // List of reference to each ShipPar
+  bool drowed;          // True if ship has been drowned
+  ShipPart * partList[];  // List of reference to each ShipPar
 };
 // New data Type ShipPart rereseting a part of a ship in one case
 struct ShipPart{
@@ -28,6 +28,8 @@ int game();
 
 // Main function called when programs is launch
 int main(int argc, char const *argv[]) {
+  printf(" ---- Main funtion ---- \n");
+
   initGame();
   game();
   return 0;
@@ -35,8 +37,50 @@ int main(int argc, char const *argv[]) {
 
 // Function to initialise game parametters
 int initGame() {
+  printf(" ---- InitGame function ---- \n");
 
+  Ship s;
+  s.drowed = false;
+  ShipPart sp1;
+  ShipPart sp2;
+  sp1.touched = false;
+  sp2.touched = false;
+  sp1.ship =  &s;
+  sp2.ship =  &s;
+  s.partList[0] = &sp1;
+  s.partList[1] = &sp2;
+  gridA[1][1] = &sp1;
+  gridA[1][2] = &sp2;
+
+  return 0;
 }
 
 // Function represneting the game itself
-int game()
+int game() {
+  printf(" ---- Game function ---- \n");
+
+  char abscisse[10] = {'A','B','C','D','E','F','G','H','I','J'};
+  printf("\n");
+  printf("   ");
+  for (int i = 0 ; i < 10; i++) {
+    printf(" %c ", abscisse[i]);
+  }
+  printf("\n");
+  printf("   ");
+  for (int i = 0 ; i < 10; i++) {
+    printf("---");
+  }
+
+  printf("\n");
+  for (int i = 0; i < 10; i++) {
+    printf("%d |",i);
+    for (int j = 0; j < 10; j++) {
+      if (gridA[i][j] == NULL) { printf(" 0 "); }
+      else { printf(" X "); }
+    }
+    printf("\n");
+  }
+  printf("\n");
+
+  return 0;
+}
